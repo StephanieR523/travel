3b069ac46a46865f9403c76f95405c9d89263e88
jeetgpatel45 <jeetgpatel45@yahoo.com>
2019-11-06 18:45:04 -0500

    added favourite page

--- client/src/pages/Favorites.js
+++ client/src/pages/Favorites.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router } from 'react-router-dom';
 import MyNavBar from '../components/Navbar';
+import FavoritesCard from "../components/Favorites/index"
 import MyCard from "../components/Card";
 import ls from "local-storage";
 
