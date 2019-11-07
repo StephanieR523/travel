f55264ff2d038e307b077a48873ad4c622d43224
Morpheus <masai.nazaire@gmail.com>
2019-11-07 10:13:10 -0500

    heroku-test2

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -59,7 +59,7 @@ class Login extends Component {
                 <b>Login</b> below
               </h4>
               <p className="grey-text text-darken-1">
-                Don't have an account? <Link to="/register">Register</Link>
+               No account? <Link to="/register">Register</Link>
               </p>
             </div>
             <form noValidate onSubmit={this.onSubmit}>
