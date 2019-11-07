1bde7602323f2fd257769addebc438af76784c0b
Morpheus <masai.nazaire@gmail.com>
2019-11-07 16:28:25 -0500

    heroku tests

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ mongoose
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
   
+
 // Passport middleware
 app.use(passport.initialize());
 // Passport config
@@ -38,9 +39,11 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+app.use(express.static(path.join(__dirname, '../client/build')));
+/*React root*/
+app.get('*', (req, res) => {
+res.sendFile(path.join(__dirname + '../client/build/index.html'));
+});
 
 
 
