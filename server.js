9448c22b498101445eec31139a13625da35595d4
Morpheus <masai.nazaire@gmail.com>
2019-11-07 15:43:45 -0500

    heroku test

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
 const cors = require('cors');
-const path = require('path');
 const app = express();
 app.use(cors())
 app.use(cors({
@@ -39,13 +38,10 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
+
 
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
