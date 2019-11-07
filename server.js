a4701c85f04e6d6553c35b16dc5423135c53ff4f
Morpheus <masai.nazaire@gmail.com>
2019-11-07 17:16:06 -0500

    slight change

--- server.js
+++ server.js
@@ -43,13 +43,18 @@ app.use("/api/users", users);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
 }
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// router.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up on port ${port} !`));
+
 
